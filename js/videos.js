281695d32e4cc59e2ae578c1273fa917ef5111cc
hai-word <3236518303@qq.com>
2026-08-07 08:29:45 +0800

    图片的

--- js/videos.js
+++ js/videos.js
@@ -1,6 +1,9 @@
 // 视频地址配置
 // 填好 src 后，页面视频窗口会播放对应地址
 window.VIDEOS = [
-    { title: '简历智写', src: '' },
-    { title: '简历优化', src: '' }
+    { title: '简历智写', src: 'https://static-jianli.jianli.jiuyeqiao.cn/assets/video/pc_index/zhixie1.mp4' },
+    { title: '简历优化', src: 'https://static-jianli.jianli.jiuyeqiao.cn/assets/video/pc_index/youhua1.mp4' },
+    { title: 'Ai面试', src: 'https://static-jianli.jianli.jiuyeqiao.cn/assets/video/pc_index/mianshi.mp4' },
+    { title: '职场伴侣', src: 'https://static-jianli.jianli.jiuyeqiao.cn/assets/video/pc_index/zhichang.mp4' },
+
 ];
